e4ef637690003b4f42ed0ca9cf2372f24b5b5734
marivin256 <marvintomusange@gmail.com>
2025-02-11 23:16:18 +0300

    minor2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flowers Gift HTML & CSS 💐
+
+Welcome! to my repository that hosts a flowers gift created in HTML & CSS so feel free to gift you special someone on various occasions 
+and you can also customize it for you needs.
+
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Have Something for u Hun'.split('')
+const text = 'This is for u Hun'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
